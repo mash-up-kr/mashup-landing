1f11d1beb41ea846a497c9cd654bf7708a2149b8
HaJunRyu <hjr4557@naver.com>
2022-02-28 02:58:55 +0900

    Feat: 채널톡 API를 추가한다.
    Changes to be committed:
    new file:   src/utils/services/ChannelService.js
    modified:   src/views/MainPage/index.tsx

--- src/views/MainPage/index.tsx
+++ src/views/MainPage/index.tsx
@@ -1,30 +1,29 @@
-import React, { useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-import S from './MainPage.module.scss';
-import PlaySection from '@views/MainPage/PlaySection';
-import TeamSection from '@views/MainPage/TeamSection';
-import ProjectSection from '@views/MainPage/ProjectSection';
-import JoinusSection from '@views/MainPage/JoinusSection';
-import HeroSection from '@views/MainPage/HeroSection';
-import Header from '@views/MainPage/Header';
-import Footer from '@views/MainPage/Footer';
-import { initializeGa } from '@utils/google';
-
+import S from "./MainPage.module.scss";
+import PlaySection from "@views/MainPage/PlaySection";
+import TeamSection from "@views/MainPage/TeamSection";
+import ProjectSection from "@views/MainPage/ProjectSection";
+import JoinusSection from "@views/MainPage/JoinusSection";
+import HeroSection from "@views/MainPage/HeroSection";
+import Header from "@views/MainPage/Header";
+import Footer from "@views/MainPage/Footer";
+import { initializeGa } from "@utils/google";
+import ChannelService from "@utils/services/ChannelService";
 
 interface Props {}
 
 const MainPage: React.FC<Props> = () => {
-
   useEffect(() => {
     initializeGa();
 
     AOS.init({
       disable: false,
-      startEvent: 'DOMContentLoaded',
-      initClassName: 'container',
-      animatedClassName: 'aos-animate',
+      startEvent: "DOMContentLoaded",
+      initClassName: "container",
+      animatedClassName: "aos-animate",
       useClassNames: true,
       disableMutationObserver: true,
       debounceDelay: 50,
@@ -32,14 +31,19 @@ const MainPage: React.FC<Props> = () => {
       offset: 120,
       delay: 0,
       duration: 400,
-      easing: 'ease',
+      easing: "ease",
       once: false,
       mirror: false,
+    });
+
+    const cService = new ChannelService();
+    cService.boot({
+      pluginKey: process.env.NEXT_PUBLIC_CHANNEL_PLUGIN,
     });
   }, []);
 
   return (
-    <div className={ S.Container }>
+    <div className={S.Container}>
       <Header />
       <HeroSection />
       <PlaySection />
@@ -48,7 +52,7 @@ const MainPage: React.FC<Props> = () => {
       <JoinusSection />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default MainPage;
