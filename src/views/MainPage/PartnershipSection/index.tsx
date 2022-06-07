5e851b738248ffd01f7569b26efef68b253e4f9f
HaJunRyu <hjr4557@naver.com>
2022-06-08 01:15:47 +0900

    Refactor: 불 필요한 fragment제거 및 문자열 따옴표 일관성 유지

--- src/views/MainPage/PartnershipSection/index.tsx
+++ src/views/MainPage/PartnershipSection/index.tsx
@@ -9,14 +9,10 @@ const PartnershipSection: React.FC<Props> = () => (
   <Section
     sectionId='partnership'
     title='매시업과 함께 하는 기업'
-    subTitle={ (
-      <>
-        매시업과 함께하는 파트너 후원사를 소개합니다.
-      </>
-    ) }
+    subTitle='매시업과 함께하는 파트너 후원사를 소개합니다.'
   >
     <div className={S.sponsorContainer}>
-      <img src={NaverCloud} alt="Naver Cloud Platform" className={S.sponsorLogo}/>
+      <img src={NaverCloud} alt='Naver Cloud Platform' className={S.sponsorLogo}/>
     </div>
   </Section>
 );
