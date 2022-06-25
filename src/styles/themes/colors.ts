75f78f47da7b71ea8c5f26fe2513e5f6e4f5bbaa
HaJunRyu <hjr4557@naver.com>
2022-06-25 21:34:59 +0900

    Refactor: color변수를 마이그레이션 한다
    Changes to be committed:
    new file:   src/styles/themes/colors.ts

---
+++ src/styles/themes/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  pageBgFirstColor: '#000000',
+  pageBgSecondColor: '#171718',
+  pageTextFirstColor: '#ffffff',
+  pageTextSecondColor: '#ced4da',
+  selectButtonColor: '#495057',
+  selectButtonFocusColor: '#06e8d3',
+  talkStartSide: '#353535',
+  talkEndSide: '#0078ff',
+} as const;
+
+export type ColorsType = typeof colors;
