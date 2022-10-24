534d2d22dc203fcac94606c6e367883c44ae3401
Baek2back <dumbuk12@naver.com>
2022-10-24 20:25:50 +0900

    Chore: 후원사 로고(하이퍼커넥트) 제거

--- src/views/MainPage/PartnershipSection/index.tsx
+++ src/views/MainPage/PartnershipSection/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Section from '@components/Section/Section';
 import naverCloud from '@resources/svg/naver_cloud.svg';
-import hyperConnect from '@resources/svg/hyperconnect.svg';
 import opgg from '@resources/images/opgg.png';
 import wanted from '@resources/images/wanted.png';
 import toss from '@resources/images/toss.png';
@@ -19,7 +18,6 @@ interface Parter {
 const partners: Parter[] = [
   { name: 'naver cloud', image: naverCloud, url: 'https://www.ncloud.com' },
   { name: 'opgg', image: opgg, url: 'https://www.op.gg' },
-  { name: 'hyper connect', image: hyperConnect, url: 'https://hyperconnect.com/ko' },
   { name: 'wanted', image: wanted, url: 'https://www.wanted.co.kr' },
   { name: 'toss', image: toss, url: 'https://toss.im' },
 ];
