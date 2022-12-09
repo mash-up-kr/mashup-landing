457858b5bdd95743e6f8832994e486d0eba1b771
HaJunRyu <hjr4557@naver.com>
2022-12-09 11:49:24 +0900

    Fix: iOS safari 환경에서 sticky로 인한 레이아웃 버그를 수정한다
    eslint autofix로 버그와 관련없는 코드의 변경도 포함되어 있습니다.

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import cc from 'classcat';
-import S from './Section.module.scss';
 import { motion } from 'framer-motion';
+import S from './Section.module.scss';
 
 interface Props {
   sectionId: string;
@@ -18,27 +18,18 @@ const Section: React.FC<Props> = ({
   hasPadding = true,
   children,
 }: Props) => (
-  <motion.section
-    id={ sectionId }
-    className={ hasPadding ? S.Container : S.ContainerWithoutPadding }
-  >
-    {
-      title && (
-        <div className={ cc([S.HeaderSection, { [S.AddPadding]: !hasPadding }])  }>
-          <h2
-            className={ S.Title }
-            data-aos='fade-up'
-            data-aos-duration='600'
-          > { title } </h2>
-          <p
-            className={ S.SubTitle }
-            data-aos='fade-up'
-            data-aos-duration='800'
-          > { subTitle } </p>
-        </div>
-      )
-    }
-    { children }
+  <motion.section id={sectionId} className={hasPadding ? S.Container : S.ContainerWithoutPadding}>
+    {title && (
+      <div className={cc([S.HeaderSection, { [S.AddPadding]: !hasPadding }])}>
+        <h2 className={S.Title} data-aos="fade-up" data-aos-duration="600">
+          {title}
+        </h2>
+        <p className={S.SubTitle} data-aos="fade-up" data-aos-duration="800">
+          {subTitle}
+        </p>
+      </div>
+    )}
+    {children}
   </motion.section>
 );
 
