520a446477d0f81ae796e977c9173009d48eda91
HaJunRyu <hjr4557@naver.com>
2022-12-23 00:21:06 +0900

    Feat: D-Day를 동적으로 변경되게 구현한다

--- src/views/MainPage/RecruitingBanner/index.tsx
+++ src/views/MainPage/RecruitingBanner/index.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import S from './styles.module.scss';
 
 const RecruitingBanner: React.FC = () => {
+  const recruitStartDate = new Date('2023-01-11T00:00:00+09:00');
+  const nowDate = new Date();
+
+  const diffMs = recruitStartDate.valueOf() - nowDate.valueOf();
+  const remaningRecruitDate = Math.ceil(diffMs / 1000 / 60 / 60 / 24);
+
   return (
     <div className={S.Container}>
       <span className={S.Message}>
         <span className={S.Season}>Mash-Up 13기</span> 리쿠르팅 Coming Soon
       </span>
       <a className={S.ApplyLink} href="https://recruit.mash-up.kr" target="_blank" rel="noreferrer">
-        D-12
+        D-{remaningRecruitDate}
       </a>
     </div>
   );
