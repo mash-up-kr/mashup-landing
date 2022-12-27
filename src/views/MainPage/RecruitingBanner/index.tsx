8c4fadd67a171b0e2f1aa53f19421f9661430793
HaJunRyu <hjr4557@naver.com>
2022-12-28 00:44:40 +0900

    Feat: 모집 시작 일정이 지났을때 RecruitingBanner의 문구를 변경한다
    테스트를 위해 임의로 모집 시작일을 2022년 12월 28일 오전 1시로 설정하였습니다.

--- src/views/MainPage/RecruitingBanner/index.tsx
+++ src/views/MainPage/RecruitingBanner/index.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import RECRUIT_ARROW from '@resources/svg/recruit_arrow.svg';
 import S from './styles.module.scss';
 
 const RecruitingBanner: React.FC = () => {
-  const recruitStartDate = new Date('2023-01-11T00:00:00+09:00');
+  const recruitStartDate = new Date('2022-12-28T01:00:00+09:00');
   const nowDate = new Date();
 
   const diffMs = recruitStartDate.valueOf() - nowDate.valueOf();
   const remainingRecruitDate = Math.ceil(diffMs / 1000 / 60 / 60 / 24);
 
-  return (
+  const isRecruitingOpen = remainingRecruitDate <= 0;
+
+  return isRecruitingOpen ? (
+    <div className={S.RecruitOpen}>
+      <span className={S.Message}>
+        <span className={S.Season}>Mash-Up 12기</span> 리쿠르팅 OPEN
+      </span>
+      <a className={S.ApplyLink} href="https://recruit.mash-up.kr" target="_blank" rel="noreferrer">
+        지원하러 가기
+        <img src={RECRUIT_ARROW} alt="" />
+      </a>
+    </div>
+  ) : (
     <a href="https://recruit.mash-up.kr" target="_blank" rel="noreferrer">
-      <div className={S.Container}>
+      <div className={S.Remaining}>
         <span className={S.Message}>
           <span className={S.Season}>Mash-Up 13기 리쿠르팅 </span>Coming Soon
         </span>
