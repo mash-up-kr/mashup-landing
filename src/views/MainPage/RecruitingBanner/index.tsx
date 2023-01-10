1f892a8eb2ef5e9defc730402893c24af4f5fae6
Baek2back <dumbuk12@naver.com>
2023-01-11 00:12:16 +0900

    Fix: 12기 -> 13기로 배너 워딩을 변경한다

--- src/views/MainPage/RecruitingBanner/index.tsx
+++ src/views/MainPage/RecruitingBanner/index.tsx
@@ -21,7 +21,7 @@ const RecruitingBanner: React.FC = () => {
   return isRecruitingOpen ? (
     <div className={S.RecruitOpen}>
       <span className={S.Message}>
-        <span className={S.Season}>Mash-Up 12기</span> 리쿠르팅 OPEN
+        <span className={S.Season}>Mash-Up 13기</span> 리쿠르팅 OPEN
       </span>
       <a className={S.ApplyLink} href="https://recruit.mash-up.kr" target="_blank" rel="noreferrer">
         지원하러 가기
