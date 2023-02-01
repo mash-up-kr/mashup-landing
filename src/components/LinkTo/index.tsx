1ddefd776dc60ec1ebd2b10ae89960d688b6bff6
HaJunRyu <hjr4557@naver.com>
2023-02-02 03:32:00 +0900

    Feat: LinkTo 컴포넌트를 정의한다
    기존 Link 컴포넌트가 className을 받을 수 없어 LinkTo 컴포넌트 내부에 a태그를 사용해주었습니다.

---
+++ src/components/LinkTo/index.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import { ReactNode, AnchorHTMLAttributes } from 'react';
+
+interface LinkToProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  href: string;
+  children: ReactNode;
+  className?: string;
+}
+
+const LinkTo = ({ href, children, className = '', ...rest }: LinkToProps) => {
+  return (
+    <Link href={href} passHref>
+      <a href={href} className={className} {...rest}>
+        {children}
+      </a>
+    </Link>
+  );
+};
+
+export default LinkTo;
